5219444c5ea6f5e68e624c271e395f509b57804a
Cintia <cin.contaadm@gmail.com>
2021-01-25 19:51:09 -0300

    Implementação da meta tag og:image

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import Head from 'next/head'
 import db from '../db.json'
 
 const GlobalStyle = createGlobalStyle`
@@ -24,11 +25,23 @@ const GlobalStyle = createGlobalStyle`
     flex-direction: column;
   }
 `
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta property="og:image" content="https://marquesfernandes.com/wp-content/uploads/2020/01/1555172.jpg"></meta>
+      </Head>
+    </div>
+  )
+}
+
 const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <IndexPage />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Component {...pageProps} />
