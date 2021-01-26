c029ae5542a9647ed53d7111574be70d224db7fe
Cintia <cin.contaadm@gmail.com>
2021-01-26 15:36:30 -0300

    Pequenas formatações e ajustes em links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const FooterWrapper = styled.footer`
   background-color: #00000070;
@@ -28,7 +29,7 @@ export default function Footer(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FooterWrapper {...props}>
-      <a href="https://www.alura.com.br/">
+      <a target="_blank" rel="noreferrer" href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
@@ -36,10 +37,10 @@ export default function Footer(props) {
         {' '}
         a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a target="_blank" rel="noreferrer" href="https://www.alura.com.br/">
           <span>Imersão React da Alura</span>
         </a>
       </p>
     </FooterWrapper>
   );
-} 
+}
